11f257a8a0f9d555e8c1882ec820527849eb6d6e
Matt Wood <matt.wood001@gmail.com>
2021-05-07 21:28:42 +0100

    initial setup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Signed in') }}</div>
-
-                    <div class="card-body">
-                        Signed in
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <main-content></main-content>
 @endsection
